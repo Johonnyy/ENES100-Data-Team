0a495a92779f8e03f8531067b6aee6778186ed55
Winski <rosstrawinski@gmail.com>
2025-11-21 14:58:36 -0500

    Tuned PID needs a bit more but good for general nav

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@ VisionSystemClient Enes100;
 // Variables 
 bool isStartingA = false;
 
+//Change for wire to detect signal
+int digitalIn = -1;
+
 // left motor #1, right motor #2
 Drive drive(1,2);
 // trigger port 2, echo port 3
@@ -24,11 +27,10 @@ void movePastObstacles();
 void setup() {
     // INITIALIZATION DON'T ALTER!!
     // Serial.begin(9600); //uncomment for serial over usb
-    Enes100.begin("Simulator", DATA, 275, 1116, 4, 5);
+    pinMode(digitalIn, INPUT);
+    Enes100.begin("Simulator", DATA, 15, 1116, 4, 5);
     delay(2000); 
     drive.begin();
-
-    drive.turnToHeading(PI/2);
 
     // Mission
     determineStartingPoint();
@@ -61,6 +63,45 @@ void moveToObjective() {
         drive.moveToPoint(0.25,1.5);
     }
 }
+
+// void startObjective() {
+//     // USE THESE
+//     // Enes100.mission(CYCLE, i); i is the duty cycle percent (ex. 10, 30, 50, 70, 90)
+//     // Enes100.mission(MAGNETISM, MAGNETIC);
+//     // Enes100.mission(MAGNETISM, NOT_MAGNETIC);
+
+//     //Lower Servo still above magnnet
+//     //Raise back and if it has light its non magnetic
+//     bool magnetic = photoresistor.isMagnetic();
+//     if(magnetic) 
+//     {
+//         Enes100.mission(MAGNETISM, MAGNETIC);
+//     } else
+//     {
+//         Enes100.mission(MAGNETISM, NOT_MAGNETIC);
+//     }
+
+//     //Go back down fully to pickup puck
+    
+//     //Report signal wave
+//     float totalDuty = 0;
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         unsigned long highTime = pulseIn(digitalIn, HIGH);
+//         unsigned long lowTime  = pulseIn(digitalIn, LOW);
+
+//         if (highTime > 0 && lowTime > 0) {
+//             float duty = (float)highTime / (highTime + lowTime);
+//             totalDuty += duty;
+//     }
+
+//     delay(20);
+//     }
+
+//     float dutyCycle = totalDuty / 5.0;
+//     Enes100.mission(CYCLE, dutyCycle);
+// }
 
 void movePastObstacles()
 {
